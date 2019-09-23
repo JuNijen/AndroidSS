2d0c9ecbcccc551b4a2754fb0eee565be4ddc2b4
JuNijen <wjstldms0823@naver.com>
2019-09-23 12:07:48 +0900

    EDIT StartReceiver (작동 확인)

--- app/src/main/java/com/example/AndroidSS/Func/StartReceiver.kt
+++ app/src/main/java/com/example/AndroidSS/Func/StartReceiver.kt
@@ -3,11 +3,17 @@ package com.example.AndroidSS.Func
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 import com.example.AndroidSS.Activity.SplashActivity
+import com.example.AndroidSS.R
 
 
 //20190920 제작
+//참고자료 ::
 //https://kitesoft.tistory.com/79
+//20190923 수정
+//참고자료 ::
+//https://heisanbug.tistory.com/5
 
 class StartReceiver : BroadcastReceiver()
 {
@@ -24,15 +30,25 @@ class StartReceiver : BroadcastReceiver()
 
         if (intentAction.equals("android.intent.action.BOOT_COMPLETED"))
         {
+
+            Toast.makeText(context, R.string.TEXT_AUDIO_RECORD_STOPPED, Toast.LENGTH_SHORT).show()
+
+
             //TODO :: SplashActivity로 해야 할 지, MainActivity로 해야할지 조금 의문.
             val intent = Intent(context, SplashActivity::class.java)
 
             //위에 만들어진 Intent에 의해 실행되는 Activity는
             //액티비티 스택에서 새로운 Task로 Activity를 실행하도록 하라는 설정.
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+            //부팅시 서비스 자동 실행의 경우 위의 AltaAutoRun를 밑의 코드로 변경하여 주시면 됩니다.
+            context?.startService(intent)
 
             //Intent에 설정된 Component(여기서는 MainActivity)를 실행
-            context?.startActivity(intent);
+            context?.startActivity(intent)
+
+            //어플리케이션을 설치 이후 한번 이상 앱을 실행 시켜 주셔야
+            //부팅시 정상적으로 앱이 실행됩니다.
         }
     }
 }
